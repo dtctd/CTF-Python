32dc1a1dcfc0aa57302fb964ce1f54948897e0d5
dtctd <udtctd@gmail.com>
2015-11-19 15:07:35 +0100

    Create PhoneLock1.py

---
+++ PhoneLock1.py
@@ -0,0 +1,18 @@
+import hashlib, itertools
+
+characters = '01234567890'
+password_length = 4
+salt=   "b4fbd04efda015a6b251d1a825ec4015"
+valid=  "e3ac60df779cf55440e188a4b827daa3"
+
+def check_password(password):
+    pw = salt + password
+    if (hashlib.md5(pw.encode('utf-8')).hexdigest()== valid):
+        print ("the phone password is " + password)
+        print (hashlib.md5(pw.encode('utf-8')).hexdigest())
+
+gen = itertools.product(characters,repeat=4)
+for password in gen:
+    check_password(''.join(password))
+
+
